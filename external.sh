7d7d79e37a7f42f3b9df86cd0bc9e3c5f447c778
Francis De Brabandere <francisdb@gmail.com>
2023-09-08 15:43:54 +0200

    build: mac only for now

--- external.sh
+++ external.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 set -e
+if [[ $OSTYPE == 'darwin'* ]]; then
+  brew install sdl2
+  brew install sdl2_gfx
+elif [[ $OSTYPE == 'linux-gnu'* ]]; then
+  sudo apt-get install -y libsdl2-dev
+  sudo apt-get install -y libsdl2-gfx-dev
+  cp cmake/libpinmame/CMakeLists_linux-x64.txt CMakeLists.txt
+fi
 rm -rf pinmame
-git clone https://github.com/vpinball/pinmame.git
+git clone --depth 1 https://github.com/vpinball/pinmame.git
 cd pinmame
 if [[ $OSTYPE == 'darwin'* ]]; then
   cp cmake/libpinmame/CMakeLists_osx-arm64.txt CMakeLists.txt
