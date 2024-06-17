400b35ad17df48fe12b79041d23c8bd57ecbd0c8
Francis De Brabandere <francisdb@gmail.com>
2024-06-17 15:06:08 +0200

    fix: macos build

--- external.sh
+++ external.sh
@@ -28,3 +28,6 @@ cmake --build build/Release -- -j$(sysctl -n hw.ncpu)
 # if [[ "${{ matrix.platform }}" == "linux-x64" ]]; then
 #   upx --best --lzma build/Release/${{ matrix.libpinmame }}
 # fi
+
+# remove the dylib files (to make sure the rust linker does not use them)
+rm -rf build/Release/*.dylib
